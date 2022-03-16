effb2f91d03d398126bfa05bc0c56f01e42486fd
Kerwen <xingchen0624@qq.com>
2022-03-16 13:33:51 +0800

    Upload wcf demo projects

---
+++ Server/CalculatorService.cs
@@ -0,0 +1,25 @@
+﻿namespace Server
+{
+    public class CalculatorService : ICalculator
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Divide(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
